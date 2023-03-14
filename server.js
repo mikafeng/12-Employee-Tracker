ce7800a51a1ba7bc5800fb1c9cd5160f91ca265e
mikafeng <mikafengler@gmail.com>
2023-03-13 22:47:28 -0400

    add db folder and files, add db connection in server.js

--- server.js
+++ server.js
@@ -13,15 +13,17 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const connection = mysql.createConnection(
+const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: '1020&Git',
+        password: '',
         database: 'employee_db'
-    }
-)
+    },
+    console.log(`Connected to the employee_db database.`)
+);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
