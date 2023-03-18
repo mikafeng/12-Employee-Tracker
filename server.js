de1ba862ac24520cd0b5dfaf9c584cd407153732
mikafeng <mikafengler@gmail.com>
2023-03-18 16:37:24 -0400

    added inquirer first prompt and switch cases for choices, test with viewDept function

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //packages 
 const express = require('express');
-const mysql = require('mysql2')
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
 const path = require('path');
 
 
@@ -20,6 +21,67 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the system_db database.`)
 );
+
+const firstPrompt = () => {
+
+    inquirer.prompt ({
+        type: 'list',
+        name: 'selection',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a deparment',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role'
+        ],
+    })
+    .then((selection) => {
+        switch (selection.selection) {
+            case 'View all departments':
+                viewDept();
+                break;
+
+            case 'View all roles':
+                viewRole();
+                break;
+
+            case 'View all employees':
+                viewEmployee();
+                break;
+
+            case 'Add a deparment':
+                addDept();
+                break;
+
+            case 'Add a role':
+                addRole();
+                break;
+
+            case 'Add an employee':
+                addEmployee();
+                break;
+
+            case 'Update an employee role':
+                updateEmployee();
+                break;
+                
+        }
+    });
+}
+
+firstPrompt();
+
+function viewDept() {
+    console.log("Viewing Departments\n");
+
+    db.query('select * from department', (err, data) => {
+        console.log(data);
+    })
+
+};
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
