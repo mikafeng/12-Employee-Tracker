8457d808b328078f1e6e385f7de3021c58913b98
mikafeng <110942241+mikafeng@users.noreply.github.com>
2023-03-19 14:23:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: '1020&Git',
+        password: '',
         database: 'system_db'
     },
     console.log(`Connected to the system_db database.`)
